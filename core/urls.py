dd5c3c911c853847ee68079892c598cb2fb36ed9
wrongwater <maciej.samborski@protonmail.com>
2026-07-08 18:10:32 +0200

    Add category list/detail views, URLs and templates

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category.html', views.category, name='category'),
+    path('categories/', views.category_list, name='category_list'),
+    path('category/<slug:slug>/', views.category_detail, name='category_detail'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.template import TemplateDoesNotExist
+from .models import Category
 
 # Create your views here.
 def index(request):
@@ -12,3 +13,14 @@ def category(request):
         return render(request, 'core/category.html')
     except TemplateDoesNotExist:
         return render(request, 'core/index.html')
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'core/category_list.html', {'categories': categories})
+
+
+def category_detail(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = category.products.filter(available=True)
+    return render(request, 'core/category_detail.html', {'category': category, 'products': products})
