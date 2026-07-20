4b041c41bb206b65469cdc426b94cc354cb6dbfc
wrongwater <maciej.samborski@protonmail.com>
2026-07-20 14:52:40 +0200

    Merge remote-tracking branch 'origin/master'

--- userauths/forms.py
+++ userauths/forms.py
@@ -1,9 +1,11 @@
-from django.contrib.auth import get_user_model
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
-# from userauths.models import User
+from userauths.models import User
+# from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
+
+# User = get_user_model()
 
 class UserRegisterForm(UserCreationForm):
     class Meta:
--- userauths/models.py
+++ userauths/models.py
@@ -1,13 +1,14 @@
+form enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# class User(AbstractUser):
-#     email = models.EmailField(max_length=255, unique=True)
-#     username = models.CharField(max_length=255, unique=True)
-#     bio = models.CharField()
-#
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = ['username']
-#
-#     def __str__(self):
-#         return self.email
+class User(AbstractUser):
+    email = models.EmailField(max_length=255, unique=True)
+    username = models.CharField(max_length=255, unique=True)
+    bio = models.CharField()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
