7d5f6bfdeca0e8e2aef6213d48a5c4739d782d45
wrongwater <maciej.samborski@protonmail.com>
2026-07-20 14:57:25 +0200

    Merge remote-tracking branch 'origin/master'

--- ecommerce_platform/urls.py
+++ ecommerce_platform/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("core.urls")),
-    path("user/", include("userauths.urls")),
+    path("userauths/", include("userauths.urls")),
 ]
 
 if settings.DEBUG:
